32e78b0d3dd969f123e1be14a337a1e903f9547e
ArmanGoodarziii <armangoodarziii21@gmail.com>
2025-12-04 14:41:42 +0330

    Create Loading.unity and Lobby.unity.
    Create animation for Loading_txt in Loading Scene.
    Create ConnectionManager.cs for manage the connection to the server.
    Create LobbyManager.cs for manage the Create or Join to Rooms.
    Create Level Scene.

---
+++ Assets/LobbyManager.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+using UnityEngine.UI;
+
+public class LobbyManager : MonoBehaviourPunCallbacks
+{
+    [Header("InputFields")] 
+    [SerializeField] private InputField createInput;
+    [SerializeField] private InputField joinInput;
+
+    public void CreateRoom()
+    {
+        PhotonNetwork.CreateRoom(createInput.text , new RoomOptions() { MaxPlayers = 4 , IsOpen = true , IsVisible = true} , TypedLobby.Default , null);
+    }
+    public void JoinRoom()
+    {
+        PhotonNetwork.JoinRoom(joinInput.text);
+    }
+    public override void OnJoinedRoom()
+    {
+        PhotonNetwork.LoadLevel("Level");
+    }
+}
