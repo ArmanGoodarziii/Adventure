f791dae2e0f2d3eaa1f2a46530950138d9aee0e5
ArmanGoodarziii <armangoodarziii21@gmail.com>
2025-12-05 11:27:19 +0330

    Fix the bug of Player animations.
    Add Enemy.cs for manage the enemy movement.
    add Item.cs for manage the collected Items.
    create collected animation and its prefab . create apple items and animation.

---
+++ Assets/Enemy.cs
@@ -0,0 +1,69 @@
+using Unity.Android.Gradle.Manifest;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private float xScale;
+    private float faceDir = -1;
+    private bool faceRight;
+    [Header("Movement")]
+    [SerializeField] private float speed;
+
+    [Header("Collision")]
+    [SerializeField] private float groundCheckDistance;
+    [SerializeField] private Transform checkNoGroundTransform;
+    [SerializeField] private float wallCheckDistance;
+    [SerializeField] private LayerMask layerGround;
+
+    private bool isGrounded;
+    private bool checkNoGround;
+    private bool isWall;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        xScale = transform.localScale.x;
+    }
+    void Update()
+    {
+        HandleCollision();
+        HandleFlip();
+    }
+    void FixedUpdate()
+    {
+        rb.linearVelocity = new Vector2(speed * faceDir, rb.linearVelocity.y);
+    }
+    private void HandleFlip()
+    {
+        if (isGrounded)
+        {
+            if(isWall || !checkNoGround)
+            {
+                faceRight = !faceRight;
+            }
+        }
+        if (faceRight)
+        {
+            faceDir = 1;
+            transform.localScale = new Vector3(-xScale , transform.localScale.y , transform.localScale.z);
+        }
+        if (!faceRight)
+        {
+            faceDir = -1;
+            transform.localScale = new Vector3(xScale , transform.localScale.y , transform.localScale.z);
+        }
+    }
+    private void HandleCollision()
+    {
+        isGrounded = Physics2D.Raycast(transform.position , Vector2.down , groundCheckDistance , layerGround);
+        checkNoGround = Physics2D.Raycast(checkNoGroundTransform.position , Vector2.down , groundCheckDistance , layerGround);
+        isWall = Physics2D.Raycast(transform.position , Vector2.right * faceDir , wallCheckDistance , layerGround);
+    }
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawLine(transform.position , new Vector2(transform.position.x , transform.position.y - groundCheckDistance));
+        Gizmos.DrawLine(checkNoGroundTransform.position , new Vector2(checkNoGroundTransform.position.x , checkNoGroundTransform.position.y - groundCheckDistance));
+        Gizmos.DrawLine(transform.position , new Vector2(transform.position.x + (faceDir * wallCheckDistance) , transform.position.y));
+    }
+}
